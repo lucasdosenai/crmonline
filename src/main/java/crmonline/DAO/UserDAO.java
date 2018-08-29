746b6164f442cbae603ac03726a58f1baa7acacc
lucasdosenai <39661894+lucasdosenai@users.noreply.github.com>
2018-08-29 12:18:46 -0300

    FINAL

--- src/main/java/crmonline/DAO/UserDAO.java
+++ src/main/java/crmonline/DAO/UserDAO.java
@@ -1,16 +1,32 @@
 package crmonline.DAO;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 import crmonline.DB.ConDB;
 import crmonline.Entidade.Usuario;
 
 public class UserDAO {
 	private Connection con;
-	String buscaLogin;
+	private String buscaLogin;
 	
 	public UserDAO() {
 		con = ConDB.getConnection();
+		buscaLogin = "SELECT USUARIO.NOME FROM USUARIO"
+				  + " WHERE USUARIO.NIF = ? AND USUARIO.SENHA = ?";
+		ArrayList<Usuario> usuarios = new ArrayList<>();
+		
+		try {
+			PreparedStatement ps = con.prepareStatement(buscaLogin);
+			ResultSet rs = ps.executeQuery();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 	
 	public Usuario buscaLogin(String usuario, String password) {
