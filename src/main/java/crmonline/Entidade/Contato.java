5e689f3ac02db3777fc964ff2ddf55bf55a6d41a
Felipe124 <felipebispoc7@outlook.com>
2018-10-08 15:57:02 -0300

    Cadastrar Contato

---
+++ src/main/java/crmonline/Entidade/Contato.java
@@ -0,0 +1,50 @@
+package crmonline.Entidade;
+
+public class Contato {
+	private ClasseGenerica nome;
+	private String telefone;
+	private String celular;
+	private String email;
+	private ClasseGenerica cliente;
+	private ClasseGenerica cargo;
+	
+	public ClasseGenerica getNome() {
+		return nome;
+	}
+	public void setNome(ClasseGenerica nome) {
+		this.nome = nome;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getCelular() {
+		return celular;
+	}
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public ClasseGenerica getCliente() {
+		return cliente;
+	}
+	public void setCliente(ClasseGenerica cliente) {
+		this.cliente = cliente;
+	}
+	public ClasseGenerica getCargo() {
+		return cargo;
+	}
+	public void setCargo(ClasseGenerica cargo) {
+		this.cargo = cargo;
+	}
+
+	
+
+}
---
+++ src/main/java/crmonline/MBean/ContatosMB.java
@@ -0,0 +1,11 @@
+package crmonline.MBean;
+
+import javax.faces.bean.ManagedBean;
+
+import crmonline.Entidade.Contato;
+
+@ManagedBean
+public class ContatosMB {
+	Contato contato;
+
+}
