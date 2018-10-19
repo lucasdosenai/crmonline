818f249047bf2d730d114806310def3c3fbb9c5e
Felipe124 <felipebispoc7@outlook.com>
2018-10-19 15:58:32 -0300

    Listando Cliente e cadastrando contato

--- src/main/java/crmonline/MBean/ContatosMB.java
+++ src/main/java/crmonline/MBean/ContatosMB.java
@@ -1,11 +1,46 @@
 package crmonline.MBean;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 
+import crmonline.DAO.ClienteDAO;
+import crmonline.Entidade.Cliente;
 import crmonline.Entidade.Contato;
 
 @ManagedBean
 public class ContatosMB {
-	Contato contato;
+	
+	private Contato contato;
+	private List<Cliente> clientes;
+	private ClienteDAO cDao;
+	
+	public ContatosMB() {
+		contato = new Contato();
+		clientes = new ArrayList<>();
+		cDao = new ClienteDAO();
+		clientes = cDao.listaCliente();
+		
+	}
+
+	public Contato getContato() {
+		return contato;
+	}
+
+	public void setContato(Contato contato) {
+		this.contato = contato;
+	}
+
+	public List<Cliente> getClientes() {
+		return clientes;
+	}
+
+	public void setClientes(List<Cliente> clientes) {
+		this.clientes = clientes;
+	}
+	
+	
+	
 
 }
