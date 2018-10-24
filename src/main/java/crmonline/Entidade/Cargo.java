6b34addd67114d4dbba14dcd92acf199ab4c026b
Felipe124 <felipebispoc7@outlook.com>
2018-10-24 16:04:50 -0300

    carga entidade

--- src/main/java/crmonline/Entidade/Cargo.java
+++ src/main/java/crmonline/Entidade/Cargo.java
@@ -2,4 +2,24 @@ package crmonline.Entidade;
 
 public class Cargo {
 
+	public Cargo() {
+
+	}
+	private Integer codigo;
+	private String nome;
+	
+	public Integer getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	
 }
