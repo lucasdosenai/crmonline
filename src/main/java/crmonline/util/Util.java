6e1a44583d0202a1e13ca06f37df8bf9b87dc52d
Bruno <42771474+INnSaNnoSs@users.noreply.github.com>
2018-11-09 10:18:10 -0300

    comit tela

--- src/main/java/crmonline/util/Util.java
+++ src/main/java/crmonline/util/Util.java
@@ -1,11 +1,20 @@
 package crmonline.util;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Random;
 
 import javax.faces.bean.ManagedBean;
 
+import crmonline.DAO.RelatorioDAO;
+
 @ManagedBean
 public class Util {
+	RelatorioDAO rDAO;
+	public Util() {
+		rDAO = new RelatorioDAO();
+	}
+	
 	public String irCadastrarNovaVisita(){
 		return "agendar?faces-redirect=true";
 	}
@@ -21,5 +30,16 @@ public class Util {
 	public String numeroAleatorio() {
 		Random random = new Random(1000);
 		return "CODIGO : " + random.nextInt(9999);
+	}
+	
+	public String converteDate(Date date) {
+		SimpleDateFormat s = new SimpleDateFormat("dd/MM/yyyy");
+		return s.format(date);
+	}
+	
+	public String mostraCliente(Integer codigo) {
+		String nomeDoCliente = "";
+		String cod = codigo.toString();
+		return nomeDoCliente = rDAO.clienteConverte(cod);
 	}
 }
